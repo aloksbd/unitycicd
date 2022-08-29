c53513f71f7a801aeccd878ad4f62668d7b13f79
aloksbd <30898579+aloksbd@users.noreply.github.com>
2022-08-29 11:43:27 +0545

    Update BuildPlayer.cs

--- creator/Assets/Editor/BuildPlayer.cs
+++ creator/Assets/Editor/BuildPlayer.cs
@@ -4,10 +4,10 @@ namespace Editor {
      {
           static void PerformBuild ()
           {
-               PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, "CREATOR");
+//                PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, "CREATOR");
                BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-               buildPlayerOptions.scenes = new[] { "Assets/Main.unity" };
-               buildPlayerOptions.locationPathName = "build";
+               buildPlayerOptions.scenes = new[] { "Assets/Scenes/Main.unity" };
+               buildPlayerOptions.locationPathName = "build/Earth9-Creator.exe";
                buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
                buildPlayerOptions.options = BuildOptions.None;
                BuildPipeline.BuildPlayer(buildPlayerOptions);
