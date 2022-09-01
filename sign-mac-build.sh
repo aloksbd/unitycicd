6efdb94686c458f71f3459d89dc3993e40e4c064
aloksbd <30898579+aloksbd@users.noreply.github.com>
2022-09-01 16:19:23 +0545

    Update sign-mac-build.sh

--- sign-mac-build.sh
+++ sign-mac-build.sh
@@ -36,15 +36,13 @@ echo "Finished setting up macOS certificate!"
 echo "Signing app..."
 sleep 10
 
-mv "${MAC_BUILD_PATH}/StandaloneOSX.app" "${MAC_BUILD_PATH}/${PROJECT_NAME}.app"
-
 chmod -R a+xr "${MAC_BUILD_PATH}/${PROJECT_NAME}.app"
 bundlepaths=$(echo $MAC_APP_BUNDLE_PATHS | tr ";" "\n")
 for bundlepath in $bundlepaths
 do
     codesign --deep --force --verbose --sign "Apple Distribution: ${APPLE_TEAM_NAME} (${APPLE_TEAM_ID})" "${MAC_BUILD_PATH}/${PROJECT_NAME}.app/$bundlepath"
 done
-codesign --deep --force --verbose --sign "Apple Distribution: ${APPLE_TEAM_NAME} (${APPLE_TEAM_ID})" --entitlements "fastlane/${PROJECT_NAME}.entitlements" "${MAC_BUILD_PATH}/${PROJECT_NAME}.app"
+codesign --deep --force --verbose --sign "Apple Distribution: ${APPLE_TEAM_NAME} (${APPLE_TEAM_ID})" --entitlements "${PROJECT_NAME}.entitlements" "${MAC_BUILD_PATH}/${PROJECT_NAME}.app"
 
 sleep 10
 echo "Packaging app..."
