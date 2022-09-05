f11dac62daa2fa70dae6fcc348217e9c20ea9fa0
aloksbd <30898579+aloksbd@users.noreply.github.com>
2022-09-05 16:42:24 +0545

    Add files via upload

---
+++ sign-bypass.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+chmod +x StandaloneOSX/Earth9-Creator/Earth9-Creator.app/Contents/MacOS/*
+xattr -cr StandaloneOSX/Earth9-Creator/Earth9-Creator.app
+
+chmod +x StandaloneOSX/Earth9/Earth9.app/Contents/MacOS/*
+xattr -cr StandaloneOSX/Earth9/Earth9.app
